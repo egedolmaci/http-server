745184f38e48361a40b4946ffaebcf458c6e8440
egedolmaci <dolmaciege@gmail.com>
2024-08-06 17:40:43 +0300

    pass stage

--- src/server.cpp
+++ src/server.cpp
@@ -1,16 +1,17 @@
 
-#include <iostream>
-#include <pthread.h>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-#include <mutex>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <cstdlib>
+#include <cstring>
 #include <fstream>
+#include <iostream>
+#include <mutex>
+#include <string>
 
 std::mutex client_mutex;
 
@@ -20,7 +21,7 @@ typedef struct {
 } thread_args;
 
 void *handle_client(void *args_ptr) {
-    thread_args *args = (thread_args *) args_ptr;
+    thread_args *args = (thread_args *)args_ptr;
     int connect_fd = args->connect_fd;
     std::string dir = args->dir;
     delete args;
@@ -39,13 +40,14 @@ void *handle_client(void *args_ptr) {
     std::string msg(buf);
 
     // Log the received message
-    std::clog << msg << "\n";
+    std::clog << "Msg: " << msg << "\n";
     if (msg[4] == '/' && msg[5] == ' ') {
         response = "HTTP/1.1 200 OK\r\n\r\n";
-    }
-    else if (msg.find("/files/") != std::string::npos) {
+    } else if (msg.find("/files/") != std::string::npos) {
         // Extract file path from the request
-        size_t start = msg.find("/files/") + 7; // Adjust to start after "/files/"
+        std::clog << "/files/ found.\n";
+        size_t start =
+            msg.find("/files/") + 7;  // Adjust to start after "/files/"
         size_t end = msg.find(' ', start);
         std::string file_to_retrieve = msg.substr(start, end - start);
         std::clog << "File to retrieve: " << file_to_retrieve << "\n";
@@ -54,11 +56,10 @@ void *handle_client(void *args_ptr) {
         std::string full_path = dir + file_to_retrieve;
         std::clog << "Full path: " << full_path << "\n";
         std::ifstream file(full_path, std::ios::binary | std::ios::ate);
-        
-        if (!file.is_open()) {
-            std::clog << "merhaba\n";
+
+        if (file.is_open()) {
             response = "HTTP/1.1 404 Not Found\r\n\r\n";
-        } else {
+        } else if (msg.find("GET") != std::string::npos) {
             // Get file size
             size_t size = file.tellg();
             file.seekg(0, std::ios::beg);
@@ -68,18 +69,36 @@ void *handle_client(void *args_ptr) {
             file.read(&file_content[0], size);
 
             // Create the response
-            response = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + std::to_string(size) + "\r\n\r\n" + file_content;
+            response =
+                "HTTP/1.1 200 OK\r\nContent-Type: "
+                "application/octet-stream\r\nContent-Length: " +
+                std::to_string(size) + "\r\n\r\n" + file_content;
+        } else if (msg.find("POST") != std::string::npos) {
+            size_t start = msg.find("\\r\\n\\r\\n");
+            size_t end = msg.find("\0");
+
+            start += 8;
+            std::string body_request = msg.substr(start, end - start);
+            std::clog << "Body Request: " << body_request << std::to_string(body_request.size())
+                      << "\n";
         }
+
     } else if (msg.find("user-agent") != std::string::npos) {
         size_t start = msg.find("User-Agent: ") + 12;
         size_t end = msg.find("\r\n", start);
         std::string content_body = msg.substr(start, end - start);
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(content_body.length()) + "\r\n\r\n" + content_body;
+        response =
+            "HTTP/1.1 200 OK\r\nContent-Type: "
+            "text/plain\r\nContent-Length: " +
+            std::to_string(content_body.length()) + "\r\n\r\n" + content_body;
     } else if (msg.find("/echo/") != std::string::npos) {
         size_t start = msg.find('/', 5);
         size_t end = msg.find(' ', start);
         std::string content_body = msg.substr(start + 1, end - start - 1);
-        response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(content_body.length()) + "\r\n\r\n" + content_body;
+        response =
+            "HTTP/1.1 200 OK\r\nContent-Type: "
+            "text/plain\r\nContent-Length: " +
+            std::to_string(content_body.length()) + "\r\n\r\n" + content_body;
     } else {
         response = "HTTP/1.1 404 Not Found\r\n\r\n";
     }
@@ -95,11 +114,10 @@ void *handle_client(void *args_ptr) {
 int main(int argc, char **argv) {
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-  
 
     std::string dir;
     if (argc == 3 && strcmp("--directory", argv[1]) == 0) {
-            dir = argv[2];
+        dir = argv[2];
     }
 
     std::cout << "Logs from your program will appear here!\n";
@@ -111,7 +129,8 @@ int main(int argc, char **argv) {
     }
 
     int reuse = 1;
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+        0) {
         std::cerr << "setsockopt failed\n";
         return 1;
     }
@@ -121,7 +140,8 @@ int main(int argc, char **argv) {
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(4221);
 
-    if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+        0) {
         std::cerr << "Failed to bind to port 4221\n";
         return 1;
     }
@@ -138,7 +158,8 @@ int main(int argc, char **argv) {
     std::cout << "Waiting for a client to connect...\n";
 
     while (true) {
-        int connect_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+        int connect_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                                (socklen_t *)&client_addr_len);
         if (connect_fd < 0) {
             std::cerr << "Accept failed\n";
             continue;
@@ -146,7 +167,7 @@ int main(int argc, char **argv) {
 
         pthread_t thread_id;
         thread_args *args = new thread_args{connect_fd, dir};
-        if (pthread_create(&thread_id, NULL, handle_client, (void*)args) < 0) {
+        if (pthread_create(&thread_id, NULL, handle_client, (void *)args) < 0) {
             std::cerr << "Could not create thread\n";
             continue;
         }
